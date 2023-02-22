ccd963659f48d21ebf43869a6de9e2404d50181a
SAMUEL BERNARDO <samuel.hagers.bernardo@gmail.com>
2023-02-22 17:03:48 -0300

    finish

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,78 @@
 
+import { useState } from 'react';
 import './App.css'
 
 function App() {
 
+  const [calc, setCalc] = useState('')
+  const [result, setResult] = useState('')
+
+  const operators = ['/', '*', '+', '-', '.'];
+
+  const updateCalc = value => {
+    const operatorsProblem = 
+    // OPERADOR SEM NUMERO OU OPERADOR APOS OPERADOR
+    operators.includes(value) && calc === "" || operators.includes(value) && operators.includes(calc.slice(-1));
+
+    if (operatorsProblem){
+      return;
+    }
+    setCalc(calc + value);
+
+    if (!operators.includes(value)) {
+      setResult(eval(calc + value).toString())
+    }
+  }
+
+  const finalCalc = () => {
+    setCalc(eval(calc).toString())
+  }
+
+  const deleteLast = () => {
+    if (calc === '') {
+      return
+    }
+    const value = calc.slice(0, -1);
+    setCalc(value)
+  }
+
+  const calculatorDigits = [7, 8, 9, 4, 5, 6, 1, 2, 3];
+  const digitsDiv = calculatorDigits.map(number =>
+      <button
+        onClick={() => updateCalc(number.toString())}
+        key={number}>{number}
+      </button>)
+
   return (
+
     <div className="App">
-      <h1>Hello World</h1>
+
+      <section className='Calculator'>
+
+        <div className='Display'>
+          {result ? <span>{`(${result})`}</span> : ''}&nbsp;
+          {calc || '0'}
+        </div>
+
+        <div className='Operators'>
+          <button onClick={() => updateCalc('/')}>/</button>
+          <button onClick={() => updateCalc('*')}>*</button>
+          <button onClick={() => updateCalc('-')}>-</button>
+          <button onClick={() => updateCalc('+')}>+</button>
+
+          <button onClick={deleteLast}>DEL</button>
+        </div>
+
+        <div className='Digits'>
+          {digitsDiv}
+          <button onClick={() => updateCalc('0')}>0</button>
+          <button onClick={() => updateCalc('.')}>.</button>
+
+          <button onClick={finalCalc}>=</button>
+
+        </div>
+
+      </section>
     </div>
   )
 }
